abce600a2b6c864e2fa44c70768f56eb125f73cc
Berend Sliedrecht <blu3beri@proton.me>
2023-08-30 09:42:19 +0200

    simplied dtor and removed dependencies
    Signed-off-by: Berend Sliedrecht <blu3beri@proton.me>

--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
-import "disposablestack/auto";
-
-export function Deconstructor() {
-  return function (target: Object, key: string) {
-    // @ts-ignore
-    if(target[key].constructor.name === "AsyncFunction") {
-      //@ts-ignore
-      target[Symbol.asyncDispose] = target[key];
-    } else {
-      //@ts-ignore
-      target[Symbol.dispose] = target[key];
-    }
-  };
+declare global {
+  export interface SymbolConstructor {
+    readonly dispose: symbol;
+    readonly asyncDispose: symbol;
+  }
 }
 
-export type Deconstructable<T> = T & {[Symbol.dispose]: () => void}
-export type AsyncDeconstructable<T> = T & {[Symbol.asyncDispose]: () => Promise<void>}
+export interface Symbol {
+  readonly [Symbol.dispose]: symbol;
+  readonly [Symbol.asyncDispose]: symbol;
+}
 
+export function Deconstructor(target: Object, key: string) {
+  // @ts-ignore
+  if (target[key].constructor.name === "AsyncFunction") {
+    //@ts-ignore
+    target[Symbol.asyncDispose] = target[key];
+  } else {
+    //@ts-ignore
+    target[Symbol.dispose] = target[key];
+  }
+}
+
+export type Deconstructable<T> = T & { [Symbol.dispose]: () => void };
+export type AsyncDeconstructable<T> = T & {
+  [Symbol.asyncDispose]: () => Promise<void>;
+};
